161856906efd9728be7e41c810a411ac6b77269d
Ron1Gam3 <fillupcupron@proton.me>
2025-02-04 16:45:33 +0100

    Update game.js

--- game.js
+++ game.js
@@ -54,6 +54,12 @@ class CoinCatcher extends Phaser.Scene {
         const hitbox = this.add.rectangle(0, 0, 120, 100, 0x000000, 0);
         this.cup.add(hitbox);
 
+        this.cupHitbox = this.physics.add.sprite(this.cup.x, this.cup.y, null);
+        this.cupHitbox.setSize(120, 100);
+        this.cupHitbox.setOffset(-60, -50);
+        this.cupHitbox.setVisible(false);
+        this.cupHitbox.setImmovable(true);
+
         this.cup.setInteractive(new Phaser.Geom.Rectangle(-70, -50, 140, 100), Phaser.Geom.Rectangle.Contains);
         this.physics.add.existing(this.cup, true);
         this.cup.body.setSize(140, 100);
@@ -102,7 +108,7 @@ class CoinCatcher extends Phaser.Scene {
         });
 
         // Add collision detection for coin collection
-        this.physics.add.overlap(this.cup, this.coins, this.collectCoin, null, this);
+        this.physics.add.overlap(this.cupHitbox, this.coins, this.collectCoin, null, this);
 
         // Add invisible floor for missed coins
         this.floor = this.add.rectangle(400, 600, 600, 20, 0x000000).setAlpha(0);
